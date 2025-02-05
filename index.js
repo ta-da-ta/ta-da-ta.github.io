0972ea9abfa5503233d2cee22b72181c74da8cd3
tadata-x <marc.giannuzzi@gmail.com>
2025-02-05 14:03:43 +0100

    first modifs

--- index.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.project-card').forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        card.style.opacity = '0.7';
-    });
-
-    card.addEventListener('mouseleave', () => {
-        card.style.opacity = '1';
-    });
-});
